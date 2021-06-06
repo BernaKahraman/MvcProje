9b2ac4dc830b4412956c7fbd6a0aff454abe5acf
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-06-06 14:12:33 +0300

    mail kontrol

--- BusinessLayer/ValidationRules/MessageValidator.cs
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -17,7 +17,6 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz");
             RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız. ");
             RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 100 karakterden fazla değer girişi yapmayın.");
-
             RuleFor(x => x.ReceiverMail).EmailAddress().WithMessage("Geçerli bir e-posta adresi giriniz!").When(i => !string.IsNullOrEmpty(i.ReceiverMail));
 
 

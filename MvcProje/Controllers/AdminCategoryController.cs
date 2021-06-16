27cc63ffca3446e8ac45dbeb2b5953f8d0f18bf6
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-06-16 22:05:05 +0300

    Rolleme

--- MvcProje/Controllers/AdminCategoryController.cs
+++ MvcProje/Controllers/AdminCategoryController.cs
@@ -15,7 +15,7 @@ namespace MvcProje.Controllers
     {
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
-        [Authorize]
+        [Authorize(Roles ="B")]
         public ActionResult Index()
         {
             var categoryvalues = cm.GetList();

0bdda729bfd22968623cb8565bfec5a9f01c8fc9
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-07-06 14:09:47 +0300

    94.Ders Sonu

---
+++ MvcProje/Controllers/ChartController.cs
@@ -0,0 +1,49 @@
+﻿using MvcProje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            return ct;
+        }
+    }
+}
---
+++ MvcProje/Models/CategoryClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProje.Models
+{
+    public class CategoryClass
+    {
+        public string CategoryName { get; set; }
+        public  int CategoryCount { get; set; }
+
+    }
+}

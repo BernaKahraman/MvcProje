c4327bb561ebeb505ddd5e0aa5b62996b20384ac
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-07-07 16:57:13 +0300

    banner

--- MvcProje/App_Start/RouteConfig.cs
+++ MvcProje/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MvcProje
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }

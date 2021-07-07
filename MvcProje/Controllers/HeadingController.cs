e8fc38a8db3110dcc0797fd9f526ab1a4ce0bc1d
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-07-07 10:39:12 +0300

    95.Ders Sonu

--- MvcProje/Controllers/HeadingController.cs
+++ MvcProje/Controllers/HeadingController.cs
@@ -21,6 +21,16 @@ namespace MvcProje.Controllers
 
             return View(headingvalues);
         }
+
+        public ActionResult HeadingReport()
+        {
+            var headingvalues = hm.GetList();
+
+            return View(headingvalues);
+
+        }
+
+
         [HttpGet]
         public ActionResult AddHeading()
         {
